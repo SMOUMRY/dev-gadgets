2cdd0f2be928af4a60823da50451b37ba59096ba
Sami Moumry <samimoumry0@gmail.com>
2023-09-19 14:55:27 +0200

    reload

--- js/script.js
+++ js/script.js
@@ -11,13 +11,12 @@ document.querySelector(".pictures-prev").addEventListener("click", function(){
 
 function getNextImg(){
     if (counter < 5) {
-        document.querySelector(".pictures-img").setAttribute("src", `img/canard-jaune-${counter + 1}-l.png`);
         counter ++;
+        document.querySelector(".pictures-img").setAttribute("src", `img/canard-jaune-${counter}-l.png`);
     }
     else{
-        counter = 0;
-        document.querySelector(".pictures-img").setAttribute("src", `img/canard-jaune-${counter + 1}-l.png`);
-        counter ++;
+        counter = 1;
+        document.querySelector(".pictures-img").setAttribute("src", `img/canard-jaune-${counter}-l.png`);
     }
 }
 
@@ -36,13 +35,53 @@ function getPreviousImg(){
 // Survol version desktop
 
 const pictures = document.querySelectorAll(".thumbs-img");
-console.log(pictures);
 
 pictures.forEach(img =>{
     img.addEventListener('mouseover', function(){
-        console.log("image");
         picture = document.querySelector(".pictures-img")
         picture.setAttribute("src", img.dataset.img);
-        console.log(img.dataset.img);
     })
 })
+
+document.querySelector('.add-cta').addEventListener('click', function(){
+    if (document.querySelector('.add-qty').value <= 99) document.querySelector('.cart-nb').textContent = document.querySelector('.add-qty').value;
+    else document.querySelector('.cart-nb').textContent = "99+"
+    document.querySelector('.add-cta').textContent = document.querySelector('.add-cta').dataset.cta
+    document.querySelector('.add-cta').classList.add("active-cta");
+    document.querySelector('.add-cta').disabled = true;
+})
+
+function getInformation() {
+    document.querySelectorAll('.product-subttl').forEach(title =>{
+        title.addEventListener('click', function(event){
+            if (event.target.classList.contains("number1")){
+                document.querySelector('.product-advantages').classList.toggle("active");
+                event.target.classList.toggle("closed");
+                localStorage.setItem('refresh', event.target.classList);
+                localStorage.setItem('hidden', document.querySelector('.number1').classList);
+            } 
+            if (event.target.classList.contains("number2")){
+                document.querySelector('.product-car').classList.toggle("active");
+                event.target.classList.toggle("closed");
+                localStorage.getItem(event.target);
+                localStorage.setItem('refresh2', document.querySelector('.number2').classList);
+            } 
+        })
+    })
+}
+
+getInformation();
+
+window.addEventListener('load', function () {
+    let reload = localStorage.getItem('refresh');
+    let reload2 = localStorage.getItem('refresh2');
+    if (reload.includes('closed')){
+        document.querySelector('.number1').classList.toggle("closed");
+        document.querySelector('.product-advantages').classList.toggle("active");
+    }
+    if (reload2.includes('closed')){
+        console.log(reload2);
+        document.querySelector('.number2').classList.toggle("closed");
+        document.querySelector('.product-car').classList.toggle("active");
+    }
+});
